109e3a792658d4723d36614af907f115dcda1963
Othman Taik <taikothman1997@gmail.com>
2020-10-23 18:01:36 +0200

    Js Not Working

--- js/script.js
+++ js/script.js
@@ -6,28 +6,20 @@ var positionEl = document.getElementById("position");
   //lista cognomi
 var surnameList = ["Asdrubale", "Favagrossa", "Miscione", "Segapeli", "Occhionero", "Padella", "Brace", "Taik", "Zappa" ]
 
-//2) Logica: elaborazione dati
-
-surnameList.push(cognomeUtente);
-surnameList.sort();
-
-//3) Inizializzazione Output: stampa elementi
+//2) Inizializzazione Output: stampa elementi
 
 buttonEl.addEventListener("click", function () {
 
     var cognomeUtente = document.getElementById("name").value;
-      surnameList.push(nomeUtente);
-      surnameList.sort();
-
-      for (i = 0; i < surnameList.length; i++) {
-        listEl.innerHTML += "<li>" + surnameList[i] + "</li>";
+    surnameList.push(nomeUtente);
+    surnameList.sort();
 
     for (i = 0; i < surnameList.length; i++) {
-      listEl.innerHTML += "<li>" + surnameList[i] + "</li>";
-    }
+         listEl.innerHTML += "<li>" + surnameList[i] + "</li>";
+       };
 
     var userPosition = 1 + surnameList.indexOf(cognomeUtente);
 
     positionEl.innerHTML = "La Posizione del cognome è la numero:" + userPosition;
 
-});
+})
