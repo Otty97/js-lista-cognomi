861febb2f2ab872ce0c88eb5cf686a2a00dd070a
Othman Taik <taikothman1997@gmail.com>
2020-10-23 15:55:53 +0200

    HTML Structure

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//1) Raccolta Input
+
+var cognomi = ["Asdrubale", "Favagrossa", "Piscione", "Segapeli", "Occhionero", "Padella", "Brace", "Taik", "Zappa" ]
+
+//2) Logica: elaborazione dati
+
+cognomi.push(cognomiUtenti);
+cognomi.sort();
+
+
+//3) Inizializzazione Output: stampa elementi
